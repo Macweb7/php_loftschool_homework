fbef51beec5b70a295bf1f375e6ef462f3c88fcd
Evgeniy Bartanov <macweb7@gmail.com>
2018-08-20 20:07:39 +0300

    Format PSR 2

--- 2/dz_1_2.php
+++ 2/dz_1_2.php
@@ -1,8 +1,8 @@
 <?php
 #Задание 2
-define ("DRAWINGS", "80");
-define ("FELTPENS", "23");
-define ("PENCILS", "40");
+define("DRAWINGS", "80");
+define("FELTPENS", "23");
+define("PENCILS", "40");
 
 $result = (DRAWINGS - (FELTPENS + PENCILS));
 echo $result;
