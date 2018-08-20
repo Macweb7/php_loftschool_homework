eb739454f6cdd25f92579959bb7e7761c497688c
Evgeniy Bartanov <macweb7@gmail.com>
2018-08-20 20:04:58 +0300

    Format PSR 2

--- dz_0.php
+++ dz_0.php
@@ -1,14 +1,12 @@
 <?php
-CONST testConstanta = 'asd';
+const TEST_CONSTANT = 'asd';
 $user_name = "Igor";
 
-function show_something() {
+function show_something()
+{
     return 'SOMETHING';
 }
 
-if (1==1) {
+if (1 == 1) {
     echo 'hi';
 }
-
-
-
