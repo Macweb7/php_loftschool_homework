458fda298da7f3e798c4e759c1cd7d49b70e72e9
Evgeniy Bartanov <macweb7@gmail.com>
2018-08-23 17:53:10 +0300

    delete dz 4,5,6

--- homework2/dz_2_4.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo date('d.m.Y H:i');
-
-$date = mktime(0, 0, 0, 02, 24, 2016);
-echo $date;
--- homework2/dz_2_5.php
+++
@@ -1,6 +0,0 @@
-<?php
-$string = 'Карл у Клары украл Кораллы';
-$string2 = 'Две бутылки лимонада';
-
-echo str_replace('К', '', $string);
-echo str_replace('Две', 'Три', $string2);
