3c2996631638347bdbfd1bde3461f2694b08fe72
Evgeniy Bartanov <macweb7@gmail.com>
2018-08-25 18:20:28 +0300

    оформил вывод

--- homework2/index.php
+++ homework2/index.php
@@ -1,12 +1,11 @@
 <?php
 require('src/functions.php');
-
-task1($a, true);
-task2('-', 4, 3, 2, 1);
-task3(2, 4);
+$string = ['Счет на оплату ЖКХ', 'Платильщик: адрес, последний платеж'];
+task1($string, false);
+task2('-', 4, 2);
+task3(10, 10);
 task4($arg);
 task5($arg);
 task6('Карл у Клары украл Кораллы');
 task7('Две бутылки лимонада');
 task8('test.txt');
-
--- homework2/src/functions.php
+++ homework2/src/functions.php
@@ -1,4 +1,9 @@
 <?php
+/*function takes_array($input)
+{
+    echo "$input[0] + $input[1] = ", $input[0]+$input[1];
+}*/
+
 function task1($arg1, $arg2)
 {
     if ((is_array($arg1)) and ($arg2 !== 'true')) {
@@ -11,27 +16,27 @@ function task1($arg1, $arg2)
 
 function task2($arg)
 {
-    $numargs = func_num_args();
-    //echo $numargs;
     $arg_list = func_get_args();
     $arithmetic_sign = $arg_list[0];
-    $result = '';
     if (!is_string($arithmetic_sign)) {
         echo "Первым параметром передайте один из арифметических знаков: +, -, *, /, 
         заключенный в одинарные кавычки.<br> Пример '+'";
     }
+    $result = '';
     if ($arithmetic_sign == '-') {
-        for ($i = 1; $i < $numargs; $i++) {
-            $result -= $arg_list[$i];
+        for ($i = 1; $i < count($arg_list); $i++) {
+            echo $arg_list[$i] + 2 . "<br>";
         }
         echo $result;
     } elseif ($arithmetic_sign == '+') {
-        for ($i = 1; $i < $numargs; $i++) {
+        unset($arg_list[0]);
+        for ($i = 1; $i < count($arg_list); $i++) {
             $result += $arg_list[$i];
         }
         echo $result;
     } elseif ($arithmetic_sign == '*') {
-        for ($i = 1; $i < $numargs; $i++) {
+        unset($arg_list[0]);
+        for ($i = 1; $i < $arg_list[$i]; $i++) {
             $result *= $arg_list[$i];
         }
         echo $result;
@@ -48,7 +53,7 @@ function task3($rows = 1, $cols = 1)
     $int_rows = is_int($rows);
     $int_cols = is_int($cols);
     if (($int_rows == true) and ($int_cols == true)) {
-        echo "<table border='homework1'>";
+        echo "<table border='1'>";
         for ($tr = 1; $tr <= $rows; $tr++) {
             echo "<tr>";
             for ($td = 1; $td <= $cols; $td++) {
@@ -61,37 +66,37 @@ function task3($rows = 1, $cols = 1)
             }
             echo "</tr>";
         }
-        echo "</table>";
+        echo "</table><br>";
     } else {
-        echo 'Введите целые числа';
+        echo "<p>Введите целые числа</p>";
     }
 }
 
 function task4($arg)
 {
-    echo date('d.m.Y H:i');
+    echo "<p>" . date('d.m.Y H:i') . "</p>";
 }
 
 function task5($arg)
 {
     $date = mktime(0, 0, 0, 02, 24, 2016);
-    echo $date;
+    echo "<p>" . $date . "</p>";
 }
 
 function task6($arg)
 {
-    echo str_replace('К', '', $arg);
+    echo "<p>" . str_replace('К', '', $arg) . "</p>";
 }
 
 function task7($arg)
 {
-    echo str_replace('Две', 'Три', $arg);
+    echo "<p>" . str_replace('Две', 'Три', $arg) . "</p>";
 }
 
 function task8($namefile)
 {
     $string = 'Hello again!';
     file_put_contents('test.txt', $string);
-    echo file_get_contents($namefile);
+    echo "<p>" . file_get_contents($namefile) . "</p>";
 }
 
